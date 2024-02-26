0b6f6f30355ce661884ff80c631464e5ca4096fb
Gabi-Pizzato <gabriela.cdossantos07@gmail.com>
2024-02-26 11:07:44 -0300

    Feat(Class): create Table Class

---
+++ src/components/UI/TableClass.vue
@@ -0,0 +1,11 @@
+<template>
+    <article class="px-7 border border-solid border-x-gray-500 border-y-gray-500 py-3 gap-4 bg-gray-800 rounded-xl">
+        <table class="text-center gap-2 w-full">
+            <tr class="text-xl text-gray-200 font-semibold">
+                <th class="py-3">Nome da aula</th>
+                <th class="py-3">Nome do módulo</th>
+            </tr>
+            <slot></slot>
+        </table>
+    </article>
+</template>
