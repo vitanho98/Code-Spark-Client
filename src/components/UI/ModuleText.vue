7ed3e0b987bafa3dce60d13a787d6ee967a4fa2d
Gabi-Pizzato <gabriela.cdossantos07@gmail.com>
2024-02-27 01:15:24 -0300

    Perf(ModuleText): improving text

--- src/components/UI/ModuleText.vue
+++ src/components/UI/ModuleText.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 
 defineProps<{
+    title: string
     text: string
+    subtitle: string
 }>()
 
 </script>
@@ -9,12 +11,12 @@ defineProps<{
 <template>
     <aside class="flex flex-col">
         <header class="gap-2 pb-7">
-            <h1 class="text-2xl text-green-500 font-righteous">Módulos</h1>
+            <h1 class="text-2xl text-green-500 font-righteous">{{title}}</h1>
         </header>
         <div class="h-auto py-3 w-full text-xl rounded-xl font-righteous bg-gray-800 text-center text-white">
             {{ text }}
         </div>
-        <span class="text-lg font-semibold font-roboto pt-7 text-white">Módulos cadastrados no curso</span>
+        <span class="text-lg font-semibold font-roboto pt-7 text-white">{{subtitle}}</span>
             <slot></slot>
     </aside>
 </template>
