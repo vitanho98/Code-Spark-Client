adae95ab37f8fcf360255a60fe49fabc48ff7811
Artur Poffo <arturpoffoc@hotmail.com>
2024-02-26 22:35:14 -0300

    Fix(Home requests): Complete home requests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,21 +18,42 @@ export default {
   data() {
     const { user } = useAuthStore()
 
+    const isAStudent = user?.role === 'STUDENT'
+
     return {
-      courses: [] as ICourseWithInstructorAndEvaluationsAverage[],
-      user
+      recentCourses: [] as ICourseWithInstructorAndEvaluationsAverage[],
+      studentCourses: [] as ICourseWithInstructorAndEvaluationsAverage[],
+      instructorCourses: [] as ICourseWithInstructorAndEvaluationsAverage[],
+      user,
+      isAStudent
     }
   },
 
   mounted() {
     this.fetchRecentCourses()
+
+    if (this.user && this.isAStudent) {
+      this.fetchStudentCourses(this.user.id)
+    } else if (this.user && !this.isAStudent) {
+      this.fetchInstructorCourses(this.user.id)
+    }
   },
 
   methods: {
     async fetchRecentCourses() {
       const response = await api.get<{ courses: ICourseWithInstructorAndEvaluationsAverage[] }>('/courses')
-      this.courses = response.data.courses
-    }
+      this.recentCourses = response.data.courses
+    },
+
+    async fetchStudentCourses(studentId: string) {
+      const response = await api.get<{ courses: ICourseWithInstructorAndEvaluationsAverage[] }>(`/students/${studentId}/enrollments`)
+      this.studentCourses = response.data.courses
+    },
+
+    async fetchInstructorCourses(instructorId: string) {
+      const response = await api.get<{ courses: ICourseWithInstructorAndEvaluationsAverage[] }>(`/instructors/${instructorId}/courses`)
+      this.instructorCourses = response.data.courses
+    },
   }
 }
 </script>
@@ -41,14 +62,19 @@ export default {
   <section id="home" class="px-4 pb-10">
     <article class="max-w-screen-2xl mx-auto max-sm:pt-12 pt-28 flex flex-col gap-8">
       <header>
-        <DefaultTitle :text="`Bem-vindo ${user?.name ?? '!'}`" />
+        <DefaultTitle :text="`${user?.name ? `Bem vindo, ${user.name}` : 'Bem vindo!'}`" />
       </header>
 
       <main class="flex flex-col gap-9">
-        <!-- Authentication is required to get this data, so for now we will use the recent courses -->
-        <ContentListWrapper title="Continue de onde parou">
-          <DefaultList class="justify-center xl:justify-start">
-            <li class="w-full xs:w-auto" v-for="course in courses" :key="course.course.id">
+        <ContentListWrapper v-if="user" :title="`${isAStudent ? 'Continue de onde parou' : 'Seus cursos'}`">
+          <DefaultList v-if="isAStudent && studentCourses.length > 0" class="justify-center xl:justify-start">
+            <li class="w-full xs:w-auto" v-for="course in studentCourses" :key="course.course.id">
+              <CourseCard :course="course" />
+            </li>
+          </DefaultList>
+
+          <DefaultList v-else-if="instructorCourses.length > 0" class="justify-center xl:justify-start">
+            <li class="w-full xs:w-auto" v-for="course in instructorCourses" :key="course.course.id">
               <CourseCard :course="course" />
             </li>
           </DefaultList>
@@ -56,7 +82,7 @@ export default {
 
         <ContentListWrapper title="Cursos recentes">
           <DefaultList class="justify-center xl:justify-start">
-            <li class="w-full xs:w-auto" v-for="course in courses" :key="course.course.id">
+            <li class="w-full xs:w-auto" v-for="course in recentCourses" :key="course.course.id">
               <CourseCard :course="course" />
             </li>
           </DefaultList>
