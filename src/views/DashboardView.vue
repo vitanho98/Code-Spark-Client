94501d8e7a4b076064c632b61dda9a900ff76f31
Gabi-Pizzato <gabriela.cdossantos07@gmail.com>
2024-02-27 11:39:43 -0300

    Feat(Dashboard): create dashboard component

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ModuleBar from '@/components/Navigation/ModuleBar.vue';
+import MetricCourseCard from '@/components/UI/MetricCourseCard.vue';
+</script>
+
+<template>
+    <div class="flex justify-between">
+        <section>
+            <ModuleBar></ModuleBar>
+        </section>
+
+
+        <section class="py-32 px-28">
+            <h1 class="text-green-500 text-2xl font-righteous">Dashboard</h1>
+            <MetricCourseCard metric-title="Média faixa etária" icon="pi pi-user" metric-name="30 anos"
+                metric-description="Média de idade">
+            </MetricCourseCard>
+        </section>
+    </div>
+</template>
